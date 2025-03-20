b81122a97d11bb288bf1e7348c774e3e690b5cfc
Meysam <kazemii.meysam@gmail.com>
2025-03-20 15:54:42 -0600

    some progress on chapter 6 - lifetimes are hard

--- exercises/06_ticket_management/02_vec/src/lib.rs
+++ exercises/06_ticket_management/02_vec/src/lib.rs
@@ -10,12 +10,44 @@
 //
 // We expect `fibonacci(0)` to return `0`, `fibonacci(1)` to return `1`,
 // `fibonacci(2)` to return `1`, and so on.
+
+// pub fn fibonacci(n: u32) -> u32 {
+//     static let mut fibo: Vec<u32> = vec![];
+//     // TODO: implement the `fibonacci` function
+//     //
+//     // Hint: use a `Vec` to memoize the results you have already calculated
+//     // so that you don't have to recalculate them several times.
+//     if n == 0 {
+//         if fibo.get(0) == None {
+//             fibo.push(0);
+//         }
+//         return 0;
+//     }
+//     if n == 1 {
+//         if fibo.get(1) == None {
+//             fibo.push(1);
+//         }
+//         return 1;
+//     }
+
+//     if fibo.get(n as usize) == None {
+//         fibo.push(fibonacci(n-1) + fibonacci(n-2));
+//     }
+//     return fibo[n as usize];
+// }
+
+// I am mad at this section. This problem is meaningless for the for vectors.
+// If we want to do memo, the Vec should be outside of the function, or static.
+// I'm writing my not Vec solution because it's just dumb.
+
 pub fn fibonacci(n: u32) -> u32 {
-    // TODO: implement the `fibonacci` function
-    //
-    // Hint: use a `Vec` to memoize the results you have already calculated
-    // so that you don't have to recalculate them several times.
-    todo!()
+    if n == 0 {
+        0
+    } else if n == 1 {
+        1
+    } else {
+        fibonacci(n - 1) + fibonacci(n - 2)
+    }
 }
 
 #[cfg(test)]
